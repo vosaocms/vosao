55a68ad363d402f068efc0d4a7605da21a4a7a7a
kinyelo <kinyelo@d7b8a73a-866d-11de-bed6-6d316fbe0868>
2009-08-27 09:38:33 +0000

    Added default page resource.

--- src/main/java/org/vosao/jsf/JSFUtil.java
+++ src/main/java/org/vosao/jsf/JSFUtil.java
@@ -1,8 +1,17 @@
 package org.vosao.jsf;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.core.io.ClassPathResource;
 
 public class JSFUtil {
 
@@ -11,6 +20,11 @@ public class JSFUtil {
 				.getExternalContext().getRequest();
 	}
 	
+	public static HttpServletResponse getResponse() {
+		return (HttpServletResponse) FacesContext.getCurrentInstance()
+				.getExternalContext().getResponse();
+	}
+
 	public static Object getSessionObject(final String name) {
 		return getRequest().getSession(true).getAttribute(name);
 	}
@@ -27,6 +41,27 @@ public class JSFUtil {
 	public static void addInfoMessage(final String msg) {
 		FacesContext.getCurrentInstance().addMessage(null, 
 				new FacesMessage(FacesMessage.SEVERITY_INFO, msg, null));
+	}
+	
+	public static void redirect(final String url) throws IOException {
+		getResponse().sendRedirect(url);
+	}
+	
+	public static String getTextResource(final String path) throws IOException {
+		ClassPathResource res = new ClassPathResource(path);
+		if (res == null) {
+			throw new IOException("Empty resource.");
+		}
+		InputStream in = res.getInputStream();
+		if (in == null) {
+			throw new IOException("Empty input stream.");
+		}
+		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+		StringBuffer buf = new StringBuffer();
+		while(reader.ready()) {
+			buf.append(reader.readLine());
+		}
+		return buf.toString();
 	}
 	
 }

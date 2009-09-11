5ffddaf0946e00e34d3a04008468a962e49a2fd1
kinyelo <kinyelo@d7b8a73a-866d-11de-bed6-6d316fbe0868>
2009-09-11 19:25:49 +0000

    Added skeleton for theme import.

--- trunk/src/main/java/org/vosao/business/TemplateBusiness.java
+++ trunk/src/main/java/org/vosao/business/TemplateBusiness.java
@@ -1,6 +1,7 @@
 package org.vosao.business;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 import org.vosao.entity.TemplateEntity;
@@ -13,4 +14,6 @@ public interface TemplateBusiness {
 	byte[] createExportFile(final List<TemplateEntity> list) throws IOException;
 	
 	List<String> validateBeforeUpdate(final TemplateEntity template);
+	
+	void importThemes(InputStream in);
 }

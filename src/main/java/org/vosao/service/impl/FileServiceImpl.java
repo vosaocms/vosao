86007c7cf995217246b59685d321264d2b10b5a8
kinyelo <kinyelo@d7b8a73a-866d-11de-bed6-6d316fbe0868>
2009-09-21 15:04:10 +0000

    Added ajax to resource deletion.

--- src/main/java/org/vosao/service/impl/FileServiceImpl.java
+++ src/main/java/org/vosao/service/impl/FileServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.vosao.entity.FileEntity;
 import org.vosao.service.FileService;
+import org.vosao.service.ServiceResponse;
 
 public class FileServiceImpl extends AbstractServiceImpl 
 		implements FileService {
@@ -11,6 +12,12 @@ public class FileServiceImpl extends AbstractServiceImpl
 	@Override
 	public List<FileEntity> getByFolder(String folderId) {
 		return getDao().getFileDao().getByFolder(folderId);
+	}
+
+	@Override
+	public ServiceResponse deleteFiles(List<String> fileIds) {
+		getDao().getFileDao().remove(fileIds);
+		return new ServiceResponse("success", "Files was successfully deleted.");
 	}
 
 }

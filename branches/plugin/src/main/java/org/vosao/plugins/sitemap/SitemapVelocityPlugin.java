58b3bcca8783dc7d6479533fa85e1fd63c9fa1e7
kinyelo <kinyelo@d7b8a73a-866d-11de-bed6-6d316fbe0868>
2010-01-28 19:43:00 +0000

    Added working VM template.

--- branches/plugin/src/main/java/org/vosao/plugins/sitemap/SitemapVelocityPlugin.java
+++ branches/plugin/src/main/java/org/vosao/plugins/sitemap/SitemapVelocityPlugin.java
@@ -1,11 +1,28 @@
 package org.vosao.plugins.sitemap;
 
+import org.apache.velocity.VelocityContext;
+import org.vosao.business.decorators.TreeItemDecorator;
+import org.vosao.entity.PageEntity;
+import org.vosao.utils.StreamUtil;
 import org.vosao.velocity.plugin.AbstractVelocityPlugin;
 
 public class SitemapVelocityPlugin extends AbstractVelocityPlugin {
 
 	public String render() {
-		return "rendered velocity plugin sitemap!!!";
+		try {
+			TreeItemDecorator<PageEntity> root = getBusiness().getPageBusiness()
+				.getTree();
+			String template = StreamUtil.getTextResource(
+				SitemapVelocityPlugin.class.getClassLoader(), 
+				"org/vosao/plugins/sitemap/sitemap.vm");
+			VelocityContext context = new VelocityContext();
+			context.put("root", root);
+			return getBusiness().getSystemService().render(template, context);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			return e.getMessage();
+		}
 	}
 	
 }

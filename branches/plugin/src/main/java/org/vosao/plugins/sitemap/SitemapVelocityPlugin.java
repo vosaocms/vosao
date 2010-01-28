e33a4a29b425f989d8485d8bee0db5fe7c5c9227
kinyelo <kinyelo@d7b8a73a-866d-11de-bed6-6d316fbe0868>
2010-01-28 16:25:51 +0000

    Added sitemap plugin

---
+++ branches/plugin/src/main/java/org/vosao/plugins/sitemap/SitemapVelocityPlugin.java
@@ -0,0 +1,11 @@
+package org.vosao.plugins.sitemap;
+
+import org.vosao.velocity.plugin.AbstractVelocityPlugin;
+
+public class SitemapVelocityPlugin extends AbstractVelocityPlugin {
+
+	public String render() {
+		return "rendered velocity plugin sitemap!!!";
+	}
+	
+}

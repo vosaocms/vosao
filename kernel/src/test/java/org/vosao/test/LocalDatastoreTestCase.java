8029301faaa2f5b40fd025ba72ee7dfe23175b88
kinyelo <kinyelo@d7b8a73a-866d-11de-bed6-6d316fbe0868>
2010-02-20 18:29:30 +0000

    Upgrade GAE 1.3.1

--- kernel/src/test/java/org/vosao/test/LocalDatastoreTestCase.java
+++ kernel/src/test/java/org/vosao/test/LocalDatastoreTestCase.java
@@ -21,27 +21,24 @@
 
 package org.vosao.test;
 
-import com.google.appengine.api.datastore.dev.LocalDatastoreService;
-import com.google.appengine.tools.development.ApiProxyLocalImpl;
-import com.google.apphosting.api.ApiProxy;
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 
 public abstract class LocalDatastoreTestCase extends LocalServiceTestCase {
 
+    private final LocalServiceTestHelper helper =
+        new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
+	
     @Override
     public void setUp() throws Exception {
         super.setUp();
-        ApiProxyLocalImpl proxy = (ApiProxyLocalImpl) ApiProxy.getDelegate();
-        proxy.setProperty(LocalDatastoreService.NO_STORAGE_PROPERTY, 
-        		Boolean.TRUE.toString());
+        helper.setUp();
     }
 
     @Override
     public void tearDown() throws Exception {
-        ApiProxyLocalImpl proxy = (ApiProxyLocalImpl) ApiProxy.getDelegate();
-        LocalDatastoreService datastoreService = 
-        	(LocalDatastoreService) proxy.getService("datastore_v3");
-        datastoreService.clearProfiles();
-        super.tearDown();
+    	//helper.tearDown();
+    	super.tearDown();
     }
 
 }

e2d66c4f3a57f25786995eb7d3ac0cddbd5c9cb4
kinyelo <kinyelo@d7b8a73a-866d-11de-bed6-6d316fbe0868>
2010-03-02 10:47:31 +0000

    removed

--- plugins/register/trunk/kernel/src/main/java/org/vosao/plugins/register/dao/RegistrationDao.java
+++
@@ -1,8 +0,0 @@
-package org.vosao.plugins.register.dao;
-
-import org.vosao.dao.BaseDao;
-import org.vosao.plugins.register.entity.RegistrationEntity;
-
-public interface RegistrationDao extends BaseDao <Long, RegistrationEntity> {
-
-}

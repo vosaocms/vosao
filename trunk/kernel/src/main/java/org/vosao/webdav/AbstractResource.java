f85fd6e4b7f3bcf169275e7d9456593fe4482658
kinyelo <kinyelo@d7b8a73a-866d-11de-bed6-6d316fbe0868>
2010-03-08 11:15:24 +0000

    Added folders listings.

--- trunk/kernel/src/main/java/org/vosao/webdav/AbstractResource.java
+++ trunk/kernel/src/main/java/org/vosao/webdav/AbstractResource.java
@@ -1,3 +1,24 @@
+/**
+ * Vosao CMS. Simple CMS for Google App Engine.
+ * Copyright (C) 2009 Vosao development team
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * email: vosao.dev@gmail.com
+ */
+
 package org.vosao.webdav;
 
 import java.util.Date;
@@ -10,11 +31,12 @@ import org.vosao.dao.Dao;
 import org.vosao.entity.UserEntity;
 
 import com.bradmcevoy.http.Auth;
+import com.bradmcevoy.http.PropFindableResource;
 import com.bradmcevoy.http.Request;
 import com.bradmcevoy.http.Resource;
 import com.bradmcevoy.http.Request.Method;
 
-public class AbstractResource implements Resource {
+public class AbstractResource implements Resource, PropFindableResource {
 
 	protected static final Log logger = LogFactory.getLog(
 			AbstractResource.class);
@@ -91,6 +113,11 @@ public class AbstractResource implements Resource {
 
 	public void setModifiedDate(Date modDate) {
 		this.modDate = modDate;
+	}
+
+	@Override
+	public Date getCreateDate() {
+		return modDate;
 	}
 
 }
--- trunk/kernel/src/main/java/org/vosao/webdav/FolderResource.java
+++ trunk/kernel/src/main/java/org/vosao/webdav/FolderResource.java
@@ -1,24 +1,57 @@
+/**
+ * Vosao CMS. Simple CMS for Google App Engine.
+ * Copyright (C) 2009 Vosao development team
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * email: vosao.dev@gmail.com
+ */
+
 package org.vosao.webdav;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 import org.vosao.business.Business;
 import org.vosao.entity.FileEntity;
 import org.vosao.entity.FolderEntity;
+import org.vosao.servlet.FolderUtil;
 
+import com.bradmcevoy.http.Auth;
 import com.bradmcevoy.http.CollectionResource;
+import com.bradmcevoy.http.GetableResource;
+import com.bradmcevoy.http.Range;
 import com.bradmcevoy.http.Resource;
+import com.bradmcevoy.http.exceptions.BadRequestException;
+import com.bradmcevoy.http.exceptions.NotAuthorizedException;
 
 public class FolderResource extends AbstractResource implements 
-		CollectionResource {
+		CollectionResource, GetableResource {
 
 	private FolderEntity folder;
+	private String path;
 	
-	public FolderResource(Business aBusiness, FolderEntity aFolder) {
+	public FolderResource(Business aBusiness, FolderEntity aFolder, 
+			String aPath) {
 		super(aBusiness, aFolder.getName(), new Date());
 		folder = aFolder;
+		path = aPath;
 	}
 
 	@Override
@@ -27,7 +60,8 @@ public class FolderResource extends AbstractResource implements
 				folder.getId());
 		for (FolderEntity child : children) {
 			if (child.getName().equals(childName)) {
-				return new FolderResource(getBusiness(), child);
+				return new FolderResource(getBusiness(), child, 
+						path + "/" + childName);
 			}
 		}
 		List<FileEntity> files = getDao().getFileDao().getByFolder(folder.getId());
@@ -45,13 +79,68 @@ public class FolderResource extends AbstractResource implements
 		List<FolderEntity> children = getDao().getFolderDao().getByParent(
 				folder.getId());
 		for (FolderEntity child : children) {
-			result.add(new FolderResource(getBusiness(), child));
+			result.add(new FolderResource(getBusiness(), child, 
+					path + "/" + child.getName()));
 		}
 		List<FileEntity> files = getDao().getFileDao().getByFolder(folder.getId());
 		for (FileEntity file : files) {
 			result.add(new FileResource(getBusiness(), file));
 		}
 		return result;
+	}
+
+	@Override
+	public Long getContentLength() {
+		return null;
+	}
+
+	@Override
+	public String getContentType(String accepts) {
+		return "text/html";
+	}
+
+	@Override
+	public Long getMaxAgeSeconds(Auth arg0) {
+		return null;
+	}
+
+	@Override
+	public void sendContent(OutputStream out, Range range,
+			Map<String, String> params, String contentType) throws IOException,
+			NotAuthorizedException, BadRequestException {
+		StringBuffer s = new StringBuffer();
+		s.append("<html><body><head>")
+			.append("<link rel=\"stylesheet\" href=\"/static/css/style.css\" type=\"text/css\" />")
+			.append("</head><h2>Vosao WebDAV site repository</h2>");
+		s.append("<table width=\"50%\" class=\"form-table\"><th width=\"40%\">Name</th><th>Size in bytes</th><th>Modified date</th></tr>");
+		List<FolderEntity> children = getDao().getFolderDao().getByParent(
+				folder.getId());
+		String relPath = path.replace("/_ah/webdav", "");
+		String basePath = relPath.length() <= 1 ? "/_ah/webdav/" : path + "/";
+		if (!folder.isRoot()) {
+			String parentPath = FolderUtil.getParentPath(path);
+			s.append("<tr><td colspan=\"3\"><img src=\"/static/images/01_left.png\"/><a href=\"")
+				.append(parentPath).append("\">Parent</a></td></tr>");
+		}
+		int i = 1;
+		for (FolderEntity child : children) {
+			s.append("<tr ").append(i++ % 2 == 1 ? "class=\"even\"" : "")
+				.append("><td colspan=\"3\"><img src=\"/static/images/folder.png\"><a href=\"")
+				.append(basePath + child.getName()).append("\">").append(child.getName())
+				.append("</a></td></tr>");
+		}
+		List<FileEntity> files = getDao().getFileDao().getByFolder(folder.getId());
+		for (FileEntity file : files) {
+			s.append("<tr ").append(i++ % 2 == 1 ? "class=\"even\"" : "")
+				.append("><td><img src=\"/static/images/page.png\" /><a href=\"")
+				.append(basePath + file.getFilename())
+				.append("\">").append(file.getFilename())
+				.append("</a></td><td>")
+				.append(file.getSize()).append("</td<td>").append(file.getLastModifiedTime())
+				.append("</td></tr>");
+		}
+		s.append("</table>");
+		out.write(s.toString().getBytes("UTF-8"));
 	}
 
 }

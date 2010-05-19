11b963f2d31185e2d70dc03f541130a6851ce182
kinyelo <kinyelo@d7b8a73a-866d-11de-bed6-6d316fbe0868>
2010-05-19 11:10:27 +0000

    Refactoring.

--- kernel/src/main/java/org/vosao/business/page/impl/StructurePageRenderDecorator.java
+++ kernel/src/main/java/org/vosao/business/page/impl/StructurePageRenderDecorator.java
@@ -19,7 +19,7 @@
  * email: vosao.dev@gmail.com
  */
 
-package org.vosao.business.impl;
+package org.vosao.business.page.impl;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;

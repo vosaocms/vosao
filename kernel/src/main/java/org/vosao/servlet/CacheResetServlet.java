4f1361dc9c9b81ec97c96d12bf8b104f569b8b97
kinyelo <kinyelo@d7b8a73a-866d-11de-bed6-6d316fbe0868>
2010-07-07 15:09:02 +0000

    http://code.google.com/p/vosao/issues/detail?id=251

---
+++ kernel/src/main/java/org/vosao/servlet/CacheResetServlet.java
@@ -0,0 +1,44 @@
+/**
+ * Vosao CMS. Simple CMS for Google App Engine.
+ * Copyright (C) 2009 Vosao development team
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * email: vosao.dev@gmail.com
+ */
+
+package org.vosao.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 
+ * @author Alexander Oleynik
+ *
+ */
+public class CacheResetServlet extends AbstractServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		getBusiness().getSystemService().getCache().clear();
+		response.setHeader("Content-type", "text/html"); 
+		response.getWriter().write("Done.");
+	}
+	
+}

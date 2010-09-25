31f070a1b17ac0d68fa10f2f50690b354e662120
kinyelo <kinyelo@d7b8a73a-866d-11de-bed6-6d316fbe0868>
2010-09-25 19:58:44 +0000

    Changed license and version.

--- plugins/disqus/trunk/src/main/java/org/vosao/plugins/disqus/DisqusVelocityPlugin.java
+++ plugins/disqus/trunk/src/main/java/org/vosao/plugins/disqus/DisqusVelocityPlugin.java
@@ -1,20 +1,21 @@
 /**
  * Vosao CMS. Simple CMS for Google App Engine.
- * Copyright (C) 2009 Vosao development team
  * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
+ * Copyright (C) 2009-2010 Vosao development team.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
  * email: vosao.dev@gmail.com
  */
@@ -24,9 +25,7 @@ package org.vosao.plugins.disqus;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.datanucleus.util.StringUtils;
+import org.apache.commons.lang.StringUtils;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.DocumentHelper;

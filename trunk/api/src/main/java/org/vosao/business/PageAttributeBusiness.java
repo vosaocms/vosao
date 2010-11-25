783ba238e147d8d6a9f3725cf3a5fb99d286bc5f
kinyelo <kinyelo@d7b8a73a-866d-11de-bed6-6d316fbe0868>
2010-11-25 20:44:05 +0000

    Finished page attributes issue 349.

--- trunk/api/src/main/java/org/vosao/business/PageAttributeBusiness.java
+++ trunk/api/src/main/java/org/vosao/business/PageAttributeBusiness.java
@@ -25,6 +25,7 @@ package org.vosao.business;
 import java.util.List;
 
 import org.vosao.entity.PageAttributeEntity;
+import org.vosao.entity.PageEntity;
 
 public interface PageAttributeBusiness {
 	
@@ -34,5 +35,16 @@ public interface PageAttributeBusiness {
 	 * @return list of found page attributes definitions.
 	 */
 	List<PageAttributeEntity> getByPage(final String pageUrl);
+	
+	/**
+	 * Get page attribute definition including inherited from parent pages.
+	 * @param pageUrl - page friendly URL.
+	 * @param name - attribute name.
+	 * @return list of found page attributes definitions.
+	 */
+	PageAttributeEntity getByPage(final String pageUrl, final String name);
+
+	void setAttribute(PageEntity page, String name, String language,
+			String value, boolean applyToChildren);
 	
 }

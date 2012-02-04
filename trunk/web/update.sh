6f6cc55e058fcc39c839d245226b696a267c51a2
kinyelo <kinyelo@d7b8a73a-866d-11de-bed6-6d316fbe0868>
2012-02-04 18:39:22 +0000

    Fixed issue 553.

--- trunk/web/update.sh
+++ trunk/web/update.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-/opt/gae/bin/appcfg.sh rollback target/vosaocms
-/opt/gae/bin/appcfg.sh update target/vosaocms
+
+APPCFG=/opt/gae/bin/appcfg.sh
+
+$APPCFG rollback target/vosaocms
+$APPCFG update target/vosaocms
+$APPCFG backends target/vosaocms rollback
+$APPCFG backends target/vosaocms update pub
+

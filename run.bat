0d471db46ec374f16fac55eeda8fa19980e1e52f
Emile <emilio24107@gmail.com>
2018-03-30 23:04:59 +0200

    bug fix : when run.bat is stopped, the dev server process is killed

---
+++ run.bat
@@ -0,0 +1,9 @@
+rem Launches vosaocms on the app engine development server.  
+
+set APPENGINE_HOME=..\..\appengine-java-sdk-1.8.1
+
+%APPENGINE_HOME%\bin\dev_appserver.cmd .\web\target\vosaocms
+
+
+pause;	   
+

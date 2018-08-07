a890d3099a2efe74a4bf87a47ec2a994a09ea2dd
Emilio <Emilio@LAPTOP-EV5D1BLM>
2018-08-07 15:15:51 +0200

    Fixed a bug in the local repo definition. Upgraded the vosao poms to 1.1. Improved build.bat. Improved classpath for Eclipse building.

--- build.bat
+++ build.bat
@@ -1,3 +1,6 @@
-call mvn -Dappname=vosao -Dmaven.test.skip=true clean package
+rem To run unit tests execution during build 
+rem -remove Dmaven.test.skip=true
 
+
+call mvn -Dappname=vosao Dmaven.test.skip=true clean package
 pause

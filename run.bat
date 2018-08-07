ca961831be1cfe96ee47ed92ad309f17449d8bea
Emile <Emile@vosaocms>
2018-08-07 23:17:58 +0200

    GAE SDK must be in the PATH

--- run.bat
+++ run.bat
@@ -1,8 +1,6 @@
 rem Launches vosaocms on the app engine development server.  
 
-set APPENGINE_HOME=..\..\appengine-java-sdk-1.8.1
-
-%APPENGINE_HOME%\bin\dev_appserver.cmd .\web\target\vosaocms
+dev_appserver.cmd .\web\target\vosaocms
 
 
 pause;	   
